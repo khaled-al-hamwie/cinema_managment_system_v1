ecac611e23bc67fa9c98ddae987479ba3bda11dd
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-06 21:47:29 +0300

    import the require modules for movies-genras

--- src/modules/movies-genras/movies-genras.module.ts
+++ src/modules/movies-genras/movies-genras.module.ts
@@ -1,12 +1,21 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { GenrasModule } from "../genras/genras.module";
+import { MoviesModule } from "../movies/movies.module";
+import { UsersModule } from "../users/users.module";
 import { MovieGenra } from "./entities/movies-genra.entity";
+import { MoviesGenrasAbilityFactory } from "./factories/movies-genras.ability.factory";
 import { MoviesGenrasController } from "./movies-genras.controller";
 import { MoviesGenrasService } from "./movies-genras.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([MovieGenra])],
+    imports: [
+        TypeOrmModule.forFeature([MovieGenra]),
+        UsersModule,
+        GenrasModule,
+        MoviesModule,
+    ],
     controllers: [MoviesGenrasController],
-    providers: [MoviesGenrasService],
+    providers: [MoviesGenrasService, MoviesGenrasAbilityFactory],
 })
 export class MoviesGenrasModule {}
