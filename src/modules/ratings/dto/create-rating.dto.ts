3b7661d46636f764488f1b52e51e06c3d7707118
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-07 23:52:47 +0300

    adding the create rating dto

--- src/modules/ratings/dto/create-rating.dto.ts
+++ src/modules/ratings/dto/create-rating.dto.ts
@@ -1,1 +1,32 @@
-export class CreateRatingDto {}
+import { Transform } from "class-transformer";
+import {
+    IsInt,
+    IsNumber,
+    IsOptional,
+    IsString,
+    Length,
+    Max,
+    Min,
+} from "class-validator";
+import { Movie } from "src/modules/movies/entities/movie.entity";
+import { UserPayloadInterface } from "src/modules/users/interfaces/user.payload.interface";
+
+export class CreateRatingDto {
+    @Transform(({ value }) => Number(value))
+    @IsInt()
+    @Min(1)
+    movie_id: number;
+
+    @IsNumber({ maxDecimalPlaces: 1, allowNaN: false, allowInfinity: false })
+    @Min(0.0)
+    @Max(10.0)
+    rating: number;
+
+    @IsOptional()
+    @IsString()
+    @Length(1, 2000)
+    comment: string;
+
+    user: UserPayloadInterface;
+    movie: Movie;
+}
