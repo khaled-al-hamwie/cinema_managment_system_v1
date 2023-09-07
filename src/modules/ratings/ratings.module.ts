ed7e7772ed274ee52be1ff13abb5d0db71896635
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-07 22:43:58 +0300

    rating module importing the required module

--- src/modules/ratings/ratings.module.ts
+++ src/modules/ratings/ratings.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { MoviesModule } from "../movies/movies.module";
+import { UsersModule } from "../users/users.module";
 import { Rating } from "./entities/rating.entity";
 import { RatingsController } from "./ratings.controller";
 import { RatingsService } from "./ratings.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Rating])],
+    imports: [TypeOrmModule.forFeature([Rating]), UsersModule, MoviesModule],
     controllers: [RatingsController],
     providers: [RatingsService],
 })
