d4aa52dc799306d03cd1256025dd5abdf1383be9
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-08 00:10:46 +0300

    fix the role ability factory file name

--- src/modules/ratings/ratings.module.ts
+++ src/modules/ratings/ratings.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { MoviesModule } from "../movies/movies.module";
 import { UsersModule } from "../users/users.module";
 import { Rating } from "./entities/rating.entity";
-import { RatingsAbilityFactory } from "./factories/roles.ability.factory";
+import { RatingsAbilityFactory } from "./factories/ratings.ability.factory";
 import { RatingsController } from "./ratings.controller";
 import { RatingsService } from "./ratings.service";
 
