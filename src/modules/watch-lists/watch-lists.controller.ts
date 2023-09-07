e8325e53683d1fc028d9a3ced33a20ba247eb42f
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-07 20:46:44 +0300

    adding the authorization to the watch list route

--- src/modules/watch-lists/watch-lists.controller.ts
+++ src/modules/watch-lists/watch-lists.controller.ts
@@ -1,23 +1,59 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseIntPipe,
+    Put,
+    UseGuards,
+} from "@nestjs/common";
+import { UserDecorator } from "src/core/decorators/user.decorator";
+import { LoggedInGuard } from "../auth/guards/logged-in.guard";
+import { UserPayloadInterface } from "../users/interfaces/user.payload.interface";
 import { CreateWatchListDto } from "./dto/create-watch-list.dto";
+import { WatchList } from "./entities/watch-list.entity";
+import { WatchListsAction } from "./enums/watch-lists.actions.enum";
 import { WatchListsService } from "./watch-lists.service";
 
+@UseGuards(LoggedInGuard)
 @Controller("watch-lists")
 export class WatchListsController {
     constructor(private readonly watchListsService: WatchListsService) {}
 
-    @Post()
-    create(@Body() createWatchListDto: CreateWatchListDto) {
+    @Put()
+    put(
+        @Body() createWatchListDto: CreateWatchListDto,
+        @UserDecorator() user: UserPayloadInterface
+    ) {
+        this.watchListsService.checkAbility(
+            WatchListsAction.PutInWatchList,
+            user,
+            WatchList
+        );
         return this.watchListsService.create(createWatchListDto);
     }
 
     @Get()
-    findAll() {
+    get(@UserDecorator() user: UserPayloadInterface) {
+        this.watchListsService.checkAbility(
+            WatchListsAction.SeeWatchList,
+            user,
+            WatchList
+        );
         return this.watchListsService.findAll();
     }
 
     @Delete(":id")
-    remove(@Param("id") id: string) {
-        return this.watchListsService.remove(+id);
+    remove(
+        @Param("id", ParseIntPipe) watch_list_id: number,
+        @UserDecorator() user: UserPayloadInterface
+    ) {
+        this.watchListsService.checkAbility(
+            WatchListsAction.RemoveFromWatchList,
+            user,
+            WatchList
+        );
+        return this.watchListsService.remove(+watch_list_id);
     }
 }
--- src/modules/watch-lists/watch-lists.service.ts
+++ src/modules/watch-lists/watch-lists.service.ts
@@ -1,8 +1,18 @@
+import { ExtractSubjectType } from "@casl/ability";
 import { Injectable } from "@nestjs/common";
+import { User } from "../users/entities/user.entity";
+import { UserUnauthorizedException } from "../users/exceptions/userUnauthorized.exception";
+import { UserPayloadInterface } from "../users/interfaces/user.payload.interface";
 import { CreateWatchListDto } from "./dto/create-watch-list.dto";
+import { WatchList } from "./entities/watch-list.entity";
+import { WatchListsAction } from "./enums/watch-lists.actions.enum";
+import { WatchListsAbilityFactory } from "./factories/watch-lists.ability.factory";
 
 @Injectable()
 export class WatchListsService {
+    constructor(
+        private readonly watchListsAbilityFactory: WatchListsAbilityFactory
+    ) {}
     create(createWatchListDto: CreateWatchListDto) {
         return "This action adds a new watchList";
     }
@@ -17,5 +27,15 @@ export class WatchListsService {
 
     remove(id: number) {
         return `This action removes a #${id} watchList`;
+    }
+
+    checkAbility(
+        action: WatchListsAction,
+        user: UserPayloadInterface | User,
+        subject?: ExtractSubjectType<typeof WatchList> | WatchList
+    ) {
+        const ability = this.watchListsAbilityFactory.createForUser(user);
+        if (ability.cannot(action, subject))
+            throw new UserUnauthorizedException();
     }
 }
