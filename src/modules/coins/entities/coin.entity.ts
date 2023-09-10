e2df2e893583a8814d2ad8cdb9b41c1e95537fdc
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-10 20:06:36 +0300

    adding the coin entity

--- src/modules/coins/entities/coin.entity.ts
+++ src/modules/coins/entities/coin.entity.ts
@@ -1,1 +1,13 @@
-export class Coin {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Coin {
+    @PrimaryGeneratedColumn({ unsigned: true, type: "tinyint" })
+    coin_id: number;
+
+    @Column({ type: "decimal", unsigned: true })
+    price: number;
+
+    @Column({ type: "decimal", unsigned: true })
+    amount: number;
+}
