325101c9f7f553b6f972e29017b66d5599e5384e
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-11 13:13:56 +0300

    adding the item entity

--- src/modules/items/entities/item.entity.ts
+++ src/modules/items/entities/item.entity.ts
@@ -1,1 +1,16 @@
-export class Item {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Item {
+    @PrimaryGeneratedColumn({ unsigned: true, type: "int" })
+    item_id: number;
+
+    @Column({ type: "varchar", length: 45 })
+    name: string;
+
+    @Column({ type: "varchar", length: 500 })
+    description: string;
+
+    @Column({ type: "decimal", unsigned: true })
+    price: number;
+}
