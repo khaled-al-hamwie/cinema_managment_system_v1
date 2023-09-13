6925e267e1d160920783b4cec493900cbbe1d253
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-13 18:57:38 +0300

    adding the item purchase service for check ability

--- src/modules/items-purchases/items-purchases.service.ts
+++ src/modules/items-purchases/items-purchases.service.ts
@@ -1,9 +1,19 @@
+import { ExtractSubjectType } from "@casl/ability";
 import { Injectable } from "@nestjs/common";
+import { User } from "../users/entities/user.entity";
+import { UserUnauthorizedException } from "../users/exceptions/userUnauthorized.exception";
+import { UserPayloadInterface } from "../users/interfaces/user.payload.interface";
 import { CreateItemsPurchaseDto } from "./dto/create-items-purchase.dto";
 import { UpdateItemsPurchaseDto } from "./dto/update-items-purchase.dto";
+import { ItemPurchase } from "./entities/items-purchase.entity";
+import { ItemsPurchasesAction } from "./enums/items-purchases.actions.enum";
+import { ItemsPurchasesAbilityFactory } from "./factories/items-purchases.ability.factory";
 
 @Injectable()
 export class ItemsPurchasesService {
+    constructor(
+        private readonly itemsPurchasesAbilityFactory: ItemsPurchasesAbilityFactory
+    ) {}
     create(createItemsPurchaseDto: CreateItemsPurchaseDto) {
         return "This action adds a new itemsPurchase";
     }
@@ -22,5 +32,15 @@ export class ItemsPurchasesService {
 
     remove(id: number) {
         return `This action removes a #${id} itemsPurchase`;
+    }
+
+    checkAbility(
+        action: ItemsPurchasesAction,
+        user: UserPayloadInterface | User,
+        subject?: ExtractSubjectType<typeof ItemPurchase> | ItemPurchase
+    ) {
+        const ability = this.itemsPurchasesAbilityFactory.createForUser(user);
+        if (ability.cannot(action, subject))
+            throw new UserUnauthorizedException();
     }
 }
