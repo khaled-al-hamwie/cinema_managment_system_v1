789c92c376928b85409f01f1b13f654a4802ceef
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-13 20:25:59 +0300

    adding the create items dto

--- src/modules/items-purchases/dto/create-items-purchase.dto.ts
+++ src/modules/items-purchases/dto/create-items-purchase.dto.ts
@@ -1,1 +1,16 @@
-export class CreateItemsPurchaseDto {}
+import { IsNumber, Min } from "class-validator";
+import { Item } from "src/modules/items/entities/item.entity";
+import { User } from "src/modules/users/entities/user.entity";
+
+export class CreateItemsPurchaseDto {
+    @IsNumber()
+    @Min(0)
+    item_id: number;
+
+    @IsNumber()
+    @Min(0)
+    amount: number;
+
+    user: User;
+    item: Item;
+}
