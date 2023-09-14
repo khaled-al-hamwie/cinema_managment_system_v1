7b453729bc6d0736ef322c1c1243e4abd88117b5
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-14 17:33:55 +0300

    adding room updated at dto

--- src/modules/rooms/dto/update-room.dto.ts
+++ src/modules/rooms/dto/update-room.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateRoomDto } from './create-room.dto';
+import { OmitType, PartialType } from "@nestjs/swagger";
+import { CreateRoomDto } from "./create-room.dto";
 
-export class UpdateRoomDto extends PartialType(CreateRoomDto) {}
+export class UpdateRoomDto extends PartialType(
+    OmitType(CreateRoomDto, ["column_count", "row_count"])
+) {}
