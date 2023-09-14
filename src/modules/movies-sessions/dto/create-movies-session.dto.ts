4055aa4f996f90b30a96ba3aef85e519ff1eb254
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-14 20:54:15 +0300

    adding the create movie session dto

--- src/modules/movies-sessions/dto/create-movies-session.dto.ts
+++ src/modules/movies-sessions/dto/create-movies-session.dto.ts
@@ -1,1 +1,41 @@
-export class CreateMoviesSessionDto {}
+import {
+    IsISO8601,
+    IsInt,
+    IsNumber,
+    IsOptional,
+    IsString,
+    Length,
+    Max,
+    Min,
+} from "class-validator";
+
+export class CreateMoviesSessionDto {
+    @IsInt()
+    @Min(0)
+    movie_id: number;
+
+    @IsInt()
+    @Min(0)
+    room_id: number;
+
+    @IsString()
+    @Length(1, 45)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(1, 500)
+    details: string;
+
+    @IsInt()
+    @Min(1)
+    @Max(500)
+    duration: number;
+
+    @IsISO8601()
+    date: string;
+
+    @IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
+    @Min(0)
+    price: number;
+}
