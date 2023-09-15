c647f3178f768887de3dea8b26bc43cb6d426928
khaled-al-hamwie <khaledalhamwiedevelopmacccount@gmail.com>
2023-09-15 21:18:21 +0300

    adding the create ticket dto

--- src/modules/tickets/dto/create-ticket.dto.ts
+++ src/modules/tickets/dto/create-ticket.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTicketDto {}
+import { Transform } from "class-transformer";
+import { IsInt, Min } from "class-validator";
+import { MovieSession } from "src/modules/movies-sessions/entities/movies-session.entity";
+import { Seet } from "src/modules/seets/entities/seet.entity";
+import { UserPayloadInterface } from "src/modules/users/interfaces/user.payload.interface";
+
+export class CreateTicketDto {
+    @Transform(({ value }) => Number(value))
+    @IsInt()
+    @Min(1)
+    movie_session_id: number;
+
+    @Transform(({ value }) => Number(value))
+    @IsInt()
+    @Min(1)
+    seet_id: number;
+
+    seet: Seet;
+
+    movie_session: MovieSession;
+
+    user: UserPayloadInterface;
+}
